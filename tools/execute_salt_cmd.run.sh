da6539b4894416cc270eb865976921067420b5e1
mcsrainbow <guosuiyu@gmail.com>
2013-07-25 03:16:49 +0000

    Update the execute_salt_cmd.run.sh

--- tools/execute_salt_cmd.run.sh
+++ tools/execute_salt_cmd.run.sh
@@ -6,19 +6,17 @@ BLACKLIST=(
 "poweroff"
 "reboot"
 "init"
-"mkfs"
-"sudo"
 )
 
 echo -ne "\033[32mExecute the command as root on all minions: \033[0m"
 read COMMAND
 echo "..."
 
-COMMAND_PREFIX=`echo ${COMMAND} | awk '{print $1}'`
-
+#COMMAND_PREFIX=`echo ${COMMAND} | awk '{print $1}'`
 for BLACKLISTED_COMMAND in ${BLACKLIST[@]} ; do
-    if [ "${BLACKLISTED_COMMAND}" == "${COMMAND_PREFIX}" ] ; then
-        echo -e "\033[31mSorry! The command \033[33m\"${COMMAND_PREFIX}\"\033[31m is blacklisted. \033[0m"
+    #if [ "${BLACKLISTED_COMMAND}" == "${COMMAND_PREFIX}" ] ; then
+    if [ `echo ${COMMAND} | grep -wc $BLACKLISTED_COMMAND` == 1 ] ; then
+        echo -e "\033[31mSorry! The command \033[33m\"${BLACKLISTED_COMMAND}\"\033[31m is blacklisted. \033[0m"
         exit 1
     fi
 done
